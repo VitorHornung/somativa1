e64d8870d2ab6b97f206ae21664e7fffda59cdd1
VitorHornung <vitorhornung@gmail.com>
2025-04-10 22:20:57 -0300

    Configuração inicial
    Configuração inicial de uma API usando o FastAPI.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# 127.0.0.1:8000/
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# 127.0.0.1:8000/teste1
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
