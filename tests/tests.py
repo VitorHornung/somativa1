e5da160e0df68dac18cb6c95e986ededf54965d3
VitorHornung <vitorhornung@gmail.com>
2025-04-25 19:19:59 -0300

    up

--- tests/tests.py
+++ tests/tests.py
@@ -11,9 +11,9 @@ async def test_root():
 
 
 @pytest.mark.asyncio
-async def funcaoteste():
-    with patch("random.randit", return_value=12345):
-        result = await funcaoteste()
+async def test_funcao_real():
+    with patch("src.main.random.randit", return_value=12345):
+        result = await funcao_real()
 
     assert result == {"teste": True, "num_aleatorio": 12345}
 
